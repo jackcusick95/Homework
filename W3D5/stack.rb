40f6b689084fa798baf178384674f6878450b05b
jackcusick95 <jackcusick95@gmail.com>
2021-03-11 22:47:07 -0500

    Add Stack Class

--- W3D5/stack.rb
+++ W3D5/stack.rb
@@ -1,15 +1,26 @@
 class Stack
 
-    def initialize
+    def initialize(array)
+        @array = array 
     end
 
     def push(el)
+        @array.push(el)
     end
 
     def pop
+        @array.pop
     end 
 
     def peek
+        @array.first
     end 
 
 end 
+
+stack = Stack.new([9,2,3,"hello", 5, "Jack"])
+
+p stack.push("yoo")
+p stack.pop 
+p stack.peek
+
