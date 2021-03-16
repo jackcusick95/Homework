bd430937fa73eea189c138eb8c881922c96c9067
jackcusick95 <jackcusick95@gmail.com>
2021-03-16 08:51:47 -0400

    Add board initialize and valid move, started make_move

--- W4D2/Mencala/lib/board.rb
+++ W4D2/Mencala/lib/board.rb
@@ -2,16 +2,39 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2 
+    @cups = Array.new(14) { Array.new }
+    place_stones
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup,idx|
+      next if idx == 6 || idx == 13
+      4.times do 
+        cup << :stone 
+      end 
+    end 
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos < 0 || start_pos > 12
+    raise "Starting cup is empty" if @cups[start_pos].empty?
+
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+    first_stone_idx = start_pos 
+    last_stone_idx = first_stone_idx + stones.length
+    @cups[first_stone_idx..last_stone_idx].each do |cup| 
+      wrap_idx = 13 % 
+      if last_stone_idx > 13
+      cup << :stone 
+    end 
+
   end
 
   def next_turn(ending_cup_idx)
